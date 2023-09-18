8d5231f4545d1e99e7044e7b2d154cdc3819280f
desokan <thefdmm@gmail.com>
2023-09-18 18:43:43 +0100

    added a universal error handler

--- app.js
+++ app.js
@@ -19,9 +19,19 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/tours", tourRouter);
 
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "fail",
-    message: `Can't find ${req.originalUrl} on this server!`,
+  const err = new Error(`Me, I Can't find ${req.originalUrl} on this server!`);
+  err.status = "fail";
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
