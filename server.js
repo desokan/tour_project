e23d8fda5c94657358dac6debc73222e1f329652
desokan <thefdmm@gmail.com>
2023-09-19 22:25:31 +0100

    implemented catching uncaught exception

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./config.env" });
 import app from "./app.js";
 
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
@@ -15,10 +21,9 @@ mongoose
   })
   .then(() => {
     console.log("DB Connection Successful");
-  })
+  });
 
 const port = process.env.PORT || 3000;
-
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
