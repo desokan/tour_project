853c4e80a17be3a7f75ad8dee108885268fe1d27
desokan <thefdmm@gmail.com>
2023-09-19 14:05:25 +0100

    implemented error handling for MongoDB CastError error type

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,45 @@
+import AppError from "../utils/appError.js";
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error("ERROR 💥", err);
+    res.status(500).json({
+      status: "error",
+      message: "Something went very wrong!",
+    });
+  }
+};
+
 export const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production ") {
+    // let error = { ...err }; // This doesn't work
+    let error = Object.create(err); // Use this.
+
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    sendErrorProd(error, res);
+  }
 };
