e96b2e4a6c03fe7b44f989fda18a64884ebc1b8a
desokan <thefdmm@gmail.com>
2023-09-20 07:21:19 +0100

    added user signup controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,21 @@
 import User from "../models/userModel";
 import { catchAsync } from "../utils/catchAsync.js";
 
+import User from "../models/userModel";
+import { catchAsync } from "../utils/catchAsync.js";
+
+export const signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      user: newUser,
+    },
+  });
+});
