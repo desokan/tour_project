538b2e1bc8d12721b10dea8eb066b82ea02df630
desokan <thefdmm@gmail.com>
2023-09-21 22:41:03 +0100

    added a get request to get all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,14 @@
-import User from "../models/userModel";
+import User from "../models/userModel.js";
+import { catchAsync } from "../utils/catchAsync.js";
+import AppError from "../utils/appError.js";
+
+export const getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+  res.status(200).json({
+    status: "success",
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
 import * as authController from "../controllers/authController.js";
+import * as userController from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.route("/signup").post(authController.signup);
 router.route("/login").post(authController.login);
+
+router.route("/").get(userController.getAllUsers);
 
 export default router;
