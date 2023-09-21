73c521c8e81aa2904a1201ab07708a313b88c096
desokan <thefdmm@gmail.com>
2023-09-21 22:35:13 +0100

    added login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js";
 import { catchAsync } from "../utils/catchAsync.js";
 import jwt from "jsonwebtoken";
+import AppError from "../utils/appError.js";
 
 export const signup = catchAsync(async (req, res, next) => {
   const { name, email, password, passwordConfirm } = req.body;
@@ -10,6 +11,8 @@ export const signup = catchAsync(async (req, res, next) => {
     password,
     passwordConfirm,
   });
+
+  newUser.password = undefined;
 
   const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
@@ -21,5 +24,26 @@ export const signup = catchAsync(async (req, res, next) => {
     data: {
       user: newUser,
     },
+  });
+});
+
+export const login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return next(new AppError("Please provide email and password!", 400));
+  }
+
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({ email }).select("+password");
+
+  // if (!user || !(await user.correctPassword(password, user.password))) {
+  //   return next(new AppError("Incorrect email or password", 401));
+  // }
+
+  // 3) If everything ok, send token to client
+  const token = "";
+  res.status(201).json({
+    status: "success",
+    token,
   });
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,8 @@ import express from "express";
 import * as authController from "../controllers/authController.js";
 
 const router = express.Router();
+
 router.route("/signup").post(authController.signup);
+router.route("/login").post(authController.login);
 
 export default router;
