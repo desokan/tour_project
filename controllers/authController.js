ffb56317ff5bfc76112b12d414cb2b5cb7efec8e
desokan <thefdmm@gmail.com>
2023-09-21 23:53:36 +0100

    used bcrypt compare to compare login and hashed password

--- controllers/authController.js
+++ controllers/authController.js
@@ -33,12 +33,11 @@ export const login = catchAsync(async (req, res, next) => {
     return next(new AppError("Please provide email and password!", 400));
   }
 
-  // 2) Check if user exists && password is correct
   const user = await User.findOne({ email }).select("+password");
 
-  // if (!user || !(await user.correctPassword(password, user.password))) {
-  //   return next(new AppError("Incorrect email or password", 401));
-  // }
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError("Incorrect email or password", 401));
+  }
 
   // 3) If everything ok, send token to client
   const token = "";
