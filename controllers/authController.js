33fec4a93ab5f640676ccb09032ebdaedc13bef0
desokan <thefdmm@gmail.com>
2023-09-21 10:29:43 +0100

    implemented authentication with jsonwebtoken

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import { catchAsync } from "../utils/catchAsync.js";
+import jwt from "jsonwebtoken";
 
 export const signup = catchAsync(async (req, res, next) => {
   const { name, email, password, passwordConfirm } = req.body;
@@ -10,8 +11,13 @@ export const signup = catchAsync(async (req, res, next) => {
     passwordConfirm,
   });
 
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
   res.status(201).json({
     status: "success",
+    token,
     data: {
       user: newUser,
     },
