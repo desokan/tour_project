5f65a4fa4e99290896a6e3127aab9ed4ba98e383
desokan <thefdmm@gmail.com>
2023-11-12 16:46:34 +0000

    implemented security

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from "express";
 import morgan from "morgan";
+import rateLimit from "express-rate-limit";
 import { requestTime } from "./middleware/reqMiddleware.js";
 import { getPublicPath } from "./utils/pathUtils.js";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import xss from "xss-clean";
+
 import AppError from "./utils/appError.js";
 import { globalErrorHandler } from "./controllers/errorController.js";
 import tourRouter from "./routes/tourRoutes.js";
@@ -10,6 +15,7 @@ import userRouter from "./routes/userRoutes.js";
 const app = express();
 
 // MIDDLEWARES
+app.use(helmet());
 app.use(express.static(getPublicPath()));
 app.use(express.json());
 app.use(requestTime);
@@ -17,6 +23,18 @@ app.use(requestTime);
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+
+app.use("/api", limiter);
+app.use(express.json({ limit: "10kb" }));
+app.use(mongoSanitize());
+app.use(xss());
 
 // ROUTES
 app.use("/api/v1/tours", tourRouter);
