5bc3e866a31961795d87747a1f2df163451f9bef
desokan <thefdmm@gmail.com>
2023-11-14 20:58:01 +0000

    implmented nested route

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -14,6 +14,9 @@ export const getAllReviews = catchAsync(async (req, res, next) => {
 });
 
 export const createReview = catchAsync(async (req, res, next) => {
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+
   const newReview = await Review.create(req.body);
 
   res.status(201).json({
